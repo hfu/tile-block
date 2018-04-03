d14bac7b7af30a8ff452c88198d15c8b1fec2303
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-04-03 12:34:37 +0000

    update

--- tile-block.js
+++ tile-block.js
@@ -23,22 +23,30 @@ app.get('/zxy/:t/:z/:x/:y', (req, res, next) => {
   [z, x, y] = [req.params.z, req.params.x, req.params.y].map(v => Number(v))
   const t = req.params.t
   console.log(req.params)
-  if(!mbtiles[t]) {
-    console.log(`${t} does not exist.`)
-    next()
-  }
-  mbtiles[req.params.t].getTile(z, x, y, (err, data, headers) => {
-    if (err) {
-      res.status(404).send(`${t}/${z}/${x}/${y} does not exist.`)
-      next()
-    } else {
-      console.log(headers)
+  getTile(t, z, x, y).then(tile => {
+    if (tile) {
       res.set('Content-Encoding', 'gzip')
       res.send(data)
-      next()
+    } else {
+      res.status(404).send(`${t}/${z}/${x}/${y} does not exist.`)
     }
   })
 })
+
+const getTile = (t, z, x, y) => {
+  return new Promise((resolve, reject) => {
+    if(!mbtiles[t]) {
+      console.log(`${t} does not exist.`)
+    }
+    mbtiles[req.params.t].getTile(z, x, y, (err, data, headers) => {
+      if (err) {
+        resolve(false)
+      } else {
+        resolve(data)
+      }
+    })
+  })
+}
 app.use(express.static('htdocs'))
 
 app.listen(3857, () => {
