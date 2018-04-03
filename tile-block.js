a5a9d24eba4540d83d8badc32ced5ec174ea759a
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-04-03 23:17:12 +0200

    update

--- tile-block.js
+++ tile-block.js
@@ -6,24 +6,26 @@ const spdy = require('spdy')
 const app = express()
 let mbtiles = {}
 
-fs.readdir('mbtiles', (err, files) => {
-  if (err) throw err
-  for (let file of files) {
-    if (file.endsWith('.mbtiles')) {
-      console.log(`loading ${file}`)
-      new MBTiles(`mbtiles/${file}?mode=ro`, (err, mbt) => {
-        if (err) throw err
-        mbtiles[file.replace('.mbtiles', '')] = mbt
-        console.log(mbtiles)
-      })
+const scanFiles = () => {
+  fs.readdir('mbtiles', (err, files) => {
+    if (err) throw err
+    for (let file of files) {
+      if (file.endsWith('.mbtiles')) {
+        const t = file.replace('.mbtiles', '')
+        if (mbtiles[t]) continue
+        new MBTiles(`mbtiles/${file}?mode=ro`, (err, mbt) => {
+          if (err) throw err
+          mbtiles[t] = mbt
+        })
+      }
     }
-  }
-})
+  })
+}
+scanFiles()
 
 app.get('/zxy/:t/:z/:x/:y', (req, res, next) => {
   const [z, x, y] = [req.params.z, req.params.x, req.params.y].map(v => Number(v))
   const t = req.params.t
-  console.log(req.params)
   getTile(t, z, x, y).then(tile => {
     if (tile) {
       res.set('Content-Encoding', 'gzip')
@@ -37,11 +39,9 @@ app.get('/zxy/:t/:z/:x/:y', (req, res, next) => {
 })
 
 const getTile = (t, z, x, y) => {
+  if (!mbtiles[t]) scanFiles()
   return new Promise((resolve, reject) => {
-    if (!mbtiles[t]) {
-      console.log(`mbtiles/${t}.mbtiles does not exist.`)
-      resolve(false)
-    }
+    if (!mbtiles[t]) resolve(false)
     mbtiles[t].getTile(z, x, y, (err, data, headers) => {
       if (err) {
         resolve(false)
@@ -63,6 +63,4 @@ spdy.createServer({
   key: fs.readFileSync('private.key'), 
   cert: fs.readFileSync('server.crt'),
   passphrase: 'tile-block'
-}, app).listen(3857, () => {
-  console.log('http/2 listening port 3857.')
-})
+}, app).listen(3857, () => { })
